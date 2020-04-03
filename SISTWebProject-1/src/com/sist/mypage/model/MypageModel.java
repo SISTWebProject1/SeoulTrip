eed37e1511345c80c2b707439fffdcb9301cd923
ung <ung6039@naver.com>
2020-04-03 12:09:03 +0900

    commit

---
+++ SISTWebProject-1/src/com/sist/mypage/model/MypageModel.java
@@ -0,0 +1,20 @@
+package com.sist.mypage.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+
+@Controller
+public class MypageModel {
+
+	@RequestMapping("mypage/profile.do")
+	public String main_main(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("main_jsp", "mypage/profile8.jsp");
+		
+		return "../index.jsp";
+	}
+
+}
