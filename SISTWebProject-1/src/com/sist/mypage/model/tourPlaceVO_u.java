d3dbe29212bca102e160684445f9961387ed4037
ung <ung6039@naver.com>
2020-04-13 14:03:02 +0900

    hfhfh

--- SISTWebProject-1/src/com/sist/mypage/model/tourPlaceVO_u.java
+++ SISTWebProject-1/src/com/sist/mypage/model/tourPlaceVO_u.java
@@ -3,5 +3,33 @@ package com.sist.mypage.model;
 public class tourPlaceVO_u {
 
 	private int no;
+	private String tname;
+	private String tphoto;
+	private String grade;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public String getTphoto() {
+		return tphoto;
+	}
+	public void setTphoto(String tphoto) {
+		this.tphoto = tphoto;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
 	
 }
