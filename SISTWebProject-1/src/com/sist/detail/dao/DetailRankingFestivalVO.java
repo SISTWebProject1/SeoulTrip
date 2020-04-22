66f3d9883e403bdd7673762308d7b04846e20e1a
caskercasker <caskercasker@daum.net>
2020-04-22 19:58:10 +0900

    디테일 info 완료

---
+++ SISTWebProject-1/src/com/sist/detail/dao/DetailRankingFestivalVO.java
@@ -0,0 +1,14 @@
+package com.sist.detail.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DetailRankingFestivalVO {
+	private int no;
+	private String fname;
+	private double avg;
+	private int cnt;
+	private int ranking;
+}
---
+++ SISTWebProject-1/src/com/sist/detail/dao/DetailRankingRestaurantVO.java
@@ -0,0 +1,14 @@
+package com.sist.detail.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DetailRankingRestaurantVO {
+	private int no;
+	private String rname;
+	private double avg;
+	private int cnt;
+	private int ranking;
+}
