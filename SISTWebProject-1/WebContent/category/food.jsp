5d4fa6100c7e7b64ae2bf219170737fe550dca71
sigk77 <sigk77@naver.com>
2020-04-24 18:38:12 +0900

    aaa

--- SISTWebProject-1/WebContent/category/food.jsp
+++ SISTWebProject-1/WebContent/category/food.jsp
@@ -5,6 +5,28 @@
 <html>
 <head>
 </head>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(function () {
+	var foodtagcode = [];
+	$('.check:checked').each(function(){
+    	foodtagcode.push($(this).val());
+    	
+		$.ajax({
+			type:"POST",
+			url:"foodtag_content.do",
+			data:{"foodtagcode":foodtagcode},
+			success:function(res)
+			{
+				$('#foodtag').html(res);
+			},
+			error: function () {
+				alert("오류");
+			}
+		})
+     });
+});
+</script>
 <body>
 	<!-- Start body Area -->
 	<div class="main-body body-gap">
@@ -12,12 +34,12 @@
 			<div class="row">
 				<div class="col-lg-3 sidebar">
 					<!-- Start food_side Area -->
-					<jsp:include page="food_side.jsp"></jsp:include>
+					<jsp:include page="food_side.jsp"/>
 					<!-- End food_side Area -->
 				</div>
-				<div class="col-lg-9 post-list">
+				<div class="col-lg-9 post-list" id="foodtag">
 					<!-- Start food_content Area -->
-					<jsp:include page="food_content.jsp"></jsp:include>
+					<jsp:include page="food_content.jsp"/>
 					<!-- End food_content Area -->
 				</div>
 			</div>
--- SISTWebProject-1/src/com/sist/category/model/FoodModel.java
+++ SISTWebProject-1/src/com/sist/category/model/FoodModel.java
@@ -51,6 +51,7 @@ public class FoodModel {
 			   endPage=allPage;
 		   
 		   
+		   
 		   // 체크박스에 테그리스트 넘기기
 		   List<FoodTagVO> taglist=FoodDAO.foodTagListData(map);
 		   System.out.println(taglist.size());
@@ -64,6 +65,9 @@ public class FoodModel {
 		   request.setAttribute("startPage", startPage);
 		   request.setAttribute("endPage", endPage);
 		   request.setAttribute("allPage", allPage);
+		   
+		   System.out.println(totalpage);
+		  
 		
 		request.setAttribute("main_jsp", "../category/food.jsp");
 		request.setAttribute("banner_on", true);
@@ -75,25 +79,66 @@ public class FoodModel {
 		  Map map=new HashMap();
 		   // 체크박스에 테그리스트 넘기기
 		   List<FoodTagVO> taglist=FoodDAO.foodTagListData(map);
-		   System.out.println(taglist.size());
 		
 		 // 태그 페이지
-		 String tpage=request.getParameter("tpage");
-		   if(tpage==null)
-			   tpage="1";
-		   int tcurpage=Integer.parseInt(tpage);
-		   int trowSize=6;
-		   int tstart=(trowSize*tcurpage)-(trowSize-1);
+		 String page=request.getParameter("page");
+		   if(page==null)
+			   page="1";
+		   int curpage=Integer.parseInt(page);
+		   int rowSize=6;
+		   int start=(rowSize*curpage)-(rowSize-1);
 
-		   int tend=trowSize*tcurpage;
+		   int end=rowSize*curpage;
+		   
+		   /*int foodtagcode = Integer.parseInt(request.getParameter("foodtagcode"));*/
+		   
 		   
 		   Map tmap=new HashMap();
-		   tmap.put("tstart", tstart);
-		   tmap.put("tend", tend);
+		   tmap.put("start", start);
+		   tmap.put("end", end);
+		   /*tmap.put("foodtagcode", foodtagcode);*/
+		   
+		   FoodDAO.foodTagDetailData(tmap);
+		   List<FoodTagDetailVO> tlist= FoodDAO.foodData(tmap);
+		   System.out.println(tlist);
+		   
+		   
+		   for(FoodTagDetailVO vo:tlist)
+		   {
+			   String rname=vo.getRname();
+			   if(rname.length()>12)
+			   {
+				   rname=rname.substring(0,12).concat("...");
+				   vo.setRname(rname);
+			   }
+		   }
+		   
+		   /*int tagtotal=FoodDAO.foodTagTotalPage(foodtagcode);*/
+		   
+		   int tagtotalpage=FoodDAO.foodTagTotalPage();
+		   
+		   final int BLOCK=10;
+		   int startPage=((curpage-1)/BLOCK*BLOCK)+1; 
+		   int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
+		   // 1~10  , 11~20  85  => endPage= 81~90
+		   
+		   int allPage=tagtotalpage;
+		   if(endPage>allPage)
+			   endPage=allPage;
+		   
+		   request.setAttribute("curpage", curpage);
+		   request.setAttribute("tagtotalpage", tagtotalpage);
+		   request.setAttribute("BLOCK", BLOCK);
+		   request.setAttribute("startPage", startPage);
+		   request.setAttribute("endPage", endPage);
+		   request.setAttribute("allPage", allPage);
 		   
 		   
 		   
-		 List<FoodTagDetailVO> tlist= FoodDAO.foodTagDetailData(tmap);
+		   System.out.println(tagtotalpage);
+
+		   
+		   
 		 request.setAttribute("tlist", tlist);
 		 request.setAttribute("taglist", taglist);
 		   
